134b2d738b6bdebd15ed1e90b1e6198c447c05fd
Henry Ly <lyhenry2004@gmail.com>
2024-07-16 22:50:47 +1200

    Created multiple levels with differing obstacles

--- ProjectBoost/scripts/Player.gd
+++ ProjectBoost/scripts/Player.gd
@@ -1,16 +1,47 @@
 extends RigidBody3D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+## The amount of force the rocket generates every frame
+@export_range(750, 3000) var thrust: float = 1000
+## The amount of toque the rocket generates every frame
+@export var torque: float = 100
+
+var is_transitioning: bool = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta * 2
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
 		
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0, 0, torque * delta))
 		
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0, 0, -torque * delta))
+
+
+func _on_body_entered(body: Node) -> void:
+	if !is_transitioning:
+		if "Goal" in body.get_groups():
+			complete_level(body.filepath)
+		if "Hazard" in body.get_groups():
+			crash_sequence()
+
+func crash_sequence() -> void:
+	print("CRASHED")
+	
+	# Disable _process function
+	set_process(false)
+	is_transitioning = true
+	
+	# Essentially make a timer, similar to await get_tree().create_timer(1).timeout
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
+
+func complete_level(next_level_file: String) -> void:
+	print("FINISHED")
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
