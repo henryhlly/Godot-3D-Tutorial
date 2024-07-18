e99c4889744785b2578075566788419c2e26078c
henryhlly <lyhenry2004@gmail.com>
2024-07-19 00:33:29 +1200

    created early instance of enemy

---
+++ BarbarianBlaster/scripts/enemy.gd
@@ -0,0 +1,7 @@
+extends PathFollow3D
+
+@export var speed = 2.5
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	progress += delta * 2.5
