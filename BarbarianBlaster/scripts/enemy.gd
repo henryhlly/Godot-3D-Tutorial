5f41175b94be1f0ae148bfeefce016e4ebce12b2
henryhlly <lyhenry2004@gmail.com>
2024-07-19 00:47:37 +1200

    Created base scene that interacts with enemy

--- BarbarianBlaster/scripts/enemy.gd
+++ BarbarianBlaster/scripts/enemy.gd
@@ -2,6 +2,10 @@ extends PathFollow3D
 
 @export var speed = 2.5
 
+@onready var base = get_tree().get_first_node_in_group("base")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress += delta * 2.5
+	if progress_ratio == 1:
+		base.take_damage()
