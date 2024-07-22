44745a85e4f9b84ed702bd73d980d71488429346
henryhlly <lyhenry2004@gmail.com>
2024-07-22 23:03:29 +1200

    Built a prototype turret model

--- BarbarianBlaster/scripts/ray_picker_camera.gd
+++ BarbarianBlaster/scripts/ray_picker_camera.gd
@@ -12,16 +12,20 @@ func _process(delta: float) -> void:
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * 100.0
 	ray_cast_3d.force_raycast_update()
 	
-	if ray_cast_3d.is_colliding:
+	if ray_cast_3d.is_colliding():
 		# Get what the ray is colliding with
 		var collider = ray_cast_3d.get_collider()
 		if collider is GridMap:
-			# Get global point where ray has collided
-			var collision_point = ray_cast_3d.get_collision_point()
-			# Get the position in terms of grid map cells
-			var cell = grid_map.local_to_map(collision_point)
-			
-			# Check if colliding cell is the free space
-			if grid_map.get_cell_item(cell) == 0:
-				# Change the cell to the turret space
-				grid_map.set_cell_item(cell, 1)
+			Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+			if Input.is_action_pressed("click"):
+				# Get global point where ray has collided
+				var collision_point = ray_cast_3d.get_collision_point()
+				# Get the position in terms of grid map cells
+				var cell = grid_map.local_to_map(collision_point)
+				
+				# Check if colliding cell is the free space
+				if grid_map.get_cell_item(cell) == 0:
+					# Change the cell to the turret space
+					grid_map.set_cell_item(cell, 1)
+	else:
+		Input.set_default_cursor_shape(Input.CURSOR_ARROW)
